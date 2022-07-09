e31e5ec1ba30bd219104c92f87201de6f7189855
mi-kobaaa <mi.kobayashi0920@gmail.com>
2022-07-09 19:24:47 +0900

    add ecs ts codes

--- lib/cdk-import-sample-stack.ts
+++ lib/cdk-import-sample-stack.ts
@@ -1,16 +1,75 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Construct } from "constructs";
 
-export class CdkImportSampleStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class EcsClusterSampleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "Private",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        }
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkImportSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc: vpc,
+      clusterName: "ecs-sample-cluster",
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "TaskDefinition"
+    );
+
+    const container = taskDefinition.addContainer("web", {
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      memoryLimitMiB: 512,
+      cpu: 256,
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: "ecs-sample",
+      }),
+    });
+
+    container.addPortMappings({
+      containerPort: 80,
+    });
+
+    const service = new ecs.FargateService(this, "Service", {
+      cluster: cluster,
+      taskDefinition: taskDefinition,
+      desiredCount: 2,
+      serviceName: `ecs-sample-service`,
+    });
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, "Alb", {
+      vpc: vpc,
+      internetFacing: true,
+      loadBalancerName: "ecs-sample-alb",
+    });
+
+    const listener = alb.addListener("Listener", {
+      port: 80,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+    });
+
+    listener.addTargets("TargetGroup", {
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      port: 3000,
+      targets: [service]
+    });
   }
 }
